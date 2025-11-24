6c1a54f4129804c0819493a6e6d796d0a0f0adc3
Frank Ng <frankng@acm.org>
2025-11-24 21:54:35 +1100

    Completed framework for consistency checks, including skeletons for ITMO Integrity checks

--- AEFConsistency/aef_consistency_check/AEFConsistencyCheck.py
+++ AEFConsistency/aef_consistency_check/AEFConsistencyCheck.py
@@ -4,13 +4,16 @@ class AEFConsistencyCheck:
     """Abstract superclass for consistency checks of AEF files.
     These checks are describedin the Article 6.2 Reference Manual (version 3, 2025)"""
 
-    def __init__(self):
+    def __init__(self, submission, cursor):
+        
+        self.submission = submission
+        self.cursor     = cursor
         return
     
-    def check(self, cursor):
+    def run(self):
         """Perform the consistency check using the provided database cursor.
         This method should be overridden by subclasses."""
-        return
+        return True
     
     def report(self):
         """Generate a report of the consistency check.
